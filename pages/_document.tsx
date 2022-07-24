54c107b0b94958cddbc091009edb73f1b680d5f9
Alan Obregon <alan.obregon49@gmail.com>
2022-07-23 23:04:19 -0300

    Agregado icon, _document y normalize css

---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initalProps = await Document.getInitialProps(ctx);
+
+    return initalProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap"
+            rel="stylesheet"
+          />
+
+          <link rel="icon" type="image/png" href="favicon.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

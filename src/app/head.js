66a65ebb983c794d80082050a1499eb493a8661a
Alan Obregón <aobregon@mailexternos.com.ar>
2023-02-13 20:24:22 -0300

    Added favicon

--- src/app/head.js
+++ src/app/head.js
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Alan Obregon</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Alan Obregon's Website" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
     </>
   )
 }

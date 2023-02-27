fb300d6d1e7ab506a2c0d3fc9f7337589d77e3b1
Alan Obregón <alan.obregon49@gmail.com>
2023-02-27 18:25:46 -0300

    Agrego hotjar para testear

--- src/app/head.js
+++ src/app/head.js
@@ -5,6 +5,20 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Alan Obregon's Website" />
       <link rel="icon" type="image/png" href="/favicon.png" />
+
+      <script 
+        dangerouslySetInnerHtml={{
+          __html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3386160,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+        }}
+      />
     </>
   )
 }

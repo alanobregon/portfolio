d96945bbbc49536d417b6641ef84eb1ce9a906c5
Alan Obregón <alan.obregon49@gmail.com>
2023-02-27 18:55:45 -0300

    Cambio codigo

--- src/app/head.js
+++ src/app/head.js
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
@@ -6,19 +8,16 @@ export default function Head() {
       <meta name="description" content="Alan Obregon's Website" />
       <link rel="icon" type="image/png" href="/favicon.png" />
 
-      <script 
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:3386160,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
-        }}
-      />
+      <Script dandangerouslySetInnerHTML={{ __html: `
+        (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:3386160,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `}}/>
     </>
   )
 }

1adfac0b91b16d24cbef514ab705c7b695b95c43
alan <aobregon@mailexternos.com.ar>
2024-11-02 16:27:50 -0300

    add tailwind. delete css files and add classes in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,30 @@
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.main}>
-      <h2 className={styles.hello}>
-        hi.
+    <div className="p-8 bg-purple-900 text-purple-200 flex flex-col justify-evenly min-h-dvh md:gap-6 xl:pl-20 xl:pr-64 2xl:pl-40 2xl:pr-96">
+      <h2 className="text-7xl md:text-8xl xl:text-9xl font-bold">
+        hello.
       </h2>
 
-      <h1 className={styles.whoami}>
-        I am Alan Obregon.
+      <h1 className="text-4xl md:text-6xl xl:text-7xl">
+        I am <span className="underline">Alan Obregon</span>.
       </h1>
 
-      <p className={styles.description}>
+      <p className="text-2xl md:text-4xl xl:text-5xl">
         I am an software developer living in Argentina.
-        At the moment I work at <a href="https://www.hiberus.com/">hiberus</a>
+        At the moment I work at <a className="font-bold underline hover:bg-purple-700" target="_blank" href="https://www.hiberus.com/">hiberus</a>
       </p>
 
-      <p className={styles.description}>
-        You can add me on <a href="https://www.linkedin.com/in/alanobregon49/">LinkedIn</a> if you want, 
-        and you can see my projects on my <a href="https://github.com/alanobregon">GitHub page</a>. 
+      <p className="text-2xl md:text-4xl xl:text-5xl">
+        You can add me on <a className="font-bold underline hover:bg-purple-700" target="_blank" href="https://www.linkedin.com/in/alanobregon49/">LinkedIn</a> if you want, 
+        and you can see my projects on my <a className="font-bold underline hover:bg-purple-700" target="_blank" href="https://github.com/alanobregon">GitHub page</a>. 
         I promise I will create more projects here! I have a lot of ideas, 
         but at the moment it is difficult to make them happen.
       </p>
 
-      <p className={styles.description}>
+      <p className="text-2xl md:text-4xl xl:text-5xl">
         Outside of programming and taking care my eight cats and two dogs
         I enjoy video games, watch series and traveling. 
       </p>
-
-      {/* <p className={styles.description}>
-        I am an independent software developer currently based in Singapore. 
-        I am the creator of the JavaScript framework Vue.js and the frontend build tool Vite. 
-        Most of my work is open source and publicly available on GitHub. 
-        If you happen to benefit from my OSS work, you can support me financially via GitHub Sponsors.
-      </p>
-
-      <p className={styles.description}>
-        You can follow me on X / Twitter where I mostly post about Vue and frontend technologies. 
-        If you happen to speak Chinese, my Chinese name is 尤雨溪 (yóu yǔ xī) and 
-        I have a Chinese-only X / Twitter alt for non-tech-focused musings.
-      </p>
-
-      <p className={styles.description}>
-        Outside of programming and helping my wife take care of our two kids, 
-        I enjoy video games (soulsborne & roguelikes are my favorite), 
-        karaoke, Japanese food, UFC/NBA/F1, and collecting watches.
-      </p> */}
     </div>
   );
 }
